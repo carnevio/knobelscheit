b6b031a113ffa196fc57fbe0ef64b4c9c213217f
Akij <user.of.replit@gmail.com>
2026-05-24 15:15:06 +0200

    fixed dice test to be correct

--- dice_test.ts
+++ dice_test.ts
@@ -1,4 +1,4 @@
-import { assertAlmostEquals, assertEquals } from "@std/assert";
+import { assertAlmostEquals, assertEquals, assert } from "@std/assert";
 import { formattedAddition } from "./demo.ts";
 import { rollDice, rollTwoDice } from './dice.ts'
 
@@ -7,8 +7,8 @@ import { rollDice, rollTwoDice } from './dice.ts'
 Deno.test(' Zahl zwischen 1-6 würfeln', () => {
     const random1 = rollDice()
     const random2 = rollDice()
-    assertAlmostEquals(random1, 3, 2)
-    assertAlmostEquals(random2, 3, 2)
+    assert((random1 > 0 && random1 < 7))
+    assert((random2 > 0 && random2 < 7))
 });
 
 Deno.test('2x Würfeln und Summe richtig auswerten', () => {
